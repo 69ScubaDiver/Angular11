6474abca529eabecb331af6ae6f7850712798165
69ScubaDiver <69ScubaDiver@gmail.com>
2021-07-23 08:54:57 +0200

    Add another sample

--- 17-TernaryOperator.js
+++ 17-TernaryOperator.js
@@ -3,8 +3,13 @@
 
 let grade = 51;
 
-let result = grade > 50 ? 'Success' : 'Fail';
+let result = (grade > 50) ? 'Success' : 'Fail';
 
 console.log("The Grade scored : [" + result + "]");
 
 
+let time = 20;
+
+let t_result = (time < 18) ? "Good day." : "Good evening.";
+
+console.log("Greeting : [" + t_result + "]");
