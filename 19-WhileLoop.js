11e8ba31fb5cc093307d6b1c1e4bc3c36fe35b1d
69ScubaDiver <69ScubaDiver@gmail.com>
2021-07-23 09:16:02 +0200

    While Loop samples

---
+++ 19-WhileLoop.js
@@ -0,0 +1,18 @@
+console.log("=== Sample 1 ===")
+
+let i=0;
+while (i<10) {
+  console.log("Hello["+i+"]")  
+  i++;
+}
+
+
+console.log("=== Sample 2 ===")
+
+const cars = ["Volkswagen", "BMW", "Volvo", "Mini"];
+
+let text = "";
+for (let x of cars) {
+  //text += x;
+  console.log(x);
+}
